9082a395d3b7a8a04a135c4cc2375ccd8dd3fed6
Dev_Skoll <me@skoll.dev>
2024-09-02 16:52:40 -0400

    Add More components for blog front-end

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,41 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>My Blog 🐾</h1>
+      <nav>
+        <RouterLink to="/">Posts</RouterLink>
+        <RouterLink :to="{ name: 'author' }">Author</RouterLink>
+        <RouterLink :to="{ name: 'post' }">Post</RouterLink>
+        <RouterLink :to="{ name: 'tag' }">Tag</RouterLink>
+      </nav>
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <RouterView />
 </template>
 
 <style scoped>
+h1 {
+  text-align: center;
+  font-weight: bold;
+  margin-bottom: 1rem;
+}
+
 header {
-  line-height: 1.5;
+  border-bottom: 1px solid #ccc;
+  margin-bottom: 1rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+nav {
+  text-align: center;
+  margin: 1rem 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+nav a {
+  padding: 0.5rem;
 }
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AuthorLink from "../components/AuthorLink.vue";
+</script>
+
+<template>
+    <h3>Post List</h3>
+    <AuthorLink />
+</template>
+
+<style scoped>
+
+</style>
